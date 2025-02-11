ccb9e26af7792b8642a0a0f1ca60f3c2c4e86d04
firas hamila <firas.hm.1996@gmail.com>
2025-02-11 20:26:18 +0100

    a

---
+++ src/components/AddExpense.jsx
@@ -0,0 +1,30 @@
+import "./AddExpense.css";
+
+const AddExpense = () => {
+  return (
+    <div className="new-expense">
+      <form>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input placeholder="Title" />
+          </div>
+          <div className="new-expense__control">
+            <label>Price</label>
+            <input placeholder="Price" type="number" min="0" step="0.01" />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input type="date" />
+          </div>
+        </div>
+        <div className="new-expense__actions">
+          <button>Cancel</button>
+          <button>Add Expense</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddExpense;
